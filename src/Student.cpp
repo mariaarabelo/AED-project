7dd020b1079a2090896fd07bc27242a4a607b866
Dinis Galvão <dacgalvao@gmail.com>
2023-10-11 00:27:37 +0100

    cnostructor de students realizado ;D

--- src/Student.cpp
+++ src/Student.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "Student.h"
+
+Student::Student(const std::string& student_name, const std::string& student_code){
+    this->student_name = student_name;
+    this->student_code = student_code;
+}
