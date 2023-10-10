92444a8430af23530954fd421fc87f59667c55a4
Alexandre Silva <asilva1604@outlook.com>
2023-10-10 15:37:30 +0100

    Added uc_constructor

--- src/UC.cpp
+++ src/UC.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "UC.h"
+
+#include <string>
+#include <vector>
+
+
+UC::UC(const std::string &uc_code, const std::vector<Class> &classes) {
+    this->uc_code = uc_code;
+    this->classes = classes;
+}
--- src/UC.h
+++ src/UC.h
@@ -10,9 +10,11 @@
 #include "Class.h"
 
 class UC {
-    private:
+private:
     std::string uc_code;
     std::vector<Class> classes;
+public:
+    UC(const std::string &uc_code, const std::vector<Class> &classes);
 };
 
 
