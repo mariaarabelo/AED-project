14f3e8c8ffbe5018777457a904cd0822cc295cd7
Alexandre Silva <asilva1604@outlook.com>
2023-10-19 10:23:07 +0100

    Created Schedule class

---
+++ src/Schedule.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexandre on 19-10-2023.
+//
+
+#include "Schedule.h"
---
+++ src/Schedule.h
@@ -0,0 +1,14 @@
+//
+// Created by alexandre on 19-10-2023.
+//
+
+#ifndef AED_PROJECT_SCHEDULE_H
+#define AED_PROJECT_SCHEDULE_H
+
+
+class Schedule {
+
+};
+
+
+#endif //AED_PROJECT_SCHEDULE_H
