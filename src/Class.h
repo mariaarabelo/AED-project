b753dd23424987e47a18f98a35501b6a674198ab
Alexandre Silva <asilva1604@outlook.com>
2023-10-22 19:54:55 +0100

    Added documentation for Class class

--- src/Class.h
+++ src/Class.h
@@ -18,12 +18,48 @@ private:
     std::vector<Lecture> lectures_;
     std::vector<std::pair<std::shared_ptr<Student>,std::list<std::string>>> enrolled_students_;
 public:
+
+    /**
+     * @brief Constructor for Class class
+     * @param class_code The code of the class
+     * @param lectures Vector with all existing lectures
+     * @param students Vector with all existing students
+     */
     Class(const std::string &class_code, const std::vector<Lecture> &lectures, const std::vector<Student>  &students);
+
+    /**
+     * @brief Getter for class_code field
+     * @return A string with the class code
+     */
     std::string class_code() const;
+
+    /**
+     * @brief Prints all lectures in class
+     */
     void printLectures() const;
+
+    /**
+     * @brief Prints all students enrolled in class
+     */
     void printEnrolledStudents() const;
+
+    /**
+     * @brief Getter for a given Lecture
+     * @param code Code of the lecture
+     * @return The Lecture
+     */
     Lecture getLecture(const std::string &code) const;
+
+    /**
+     * @brief Getter of all lectures
+     * @return const reference to all lectures
+     */
     const std::vector<Lecture> &lectures() const;
+
+    /**
+     * @brief Number of enrolled students in class
+     * @return size_t with enrolled students
+     */
     size_t countEnrolledStudents() const;
 };
 
