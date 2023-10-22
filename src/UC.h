28e87f799a2eecdd133bb64134f9249178960fd5
Alexandre Silva <asilva1604@outlook.com>
2023-10-22 19:51:20 +0100

    Added documentation for UC class

--- src/UC.h
+++ src/UC.h
@@ -18,14 +18,55 @@ private:
     std::list<std::shared_ptr<Class>> classes_;
     std::list<std::shared_ptr<Student>> enrolled_students_;
 public:
+
+    /**
+     * @brief Constructor for UC class
+     * This function instantiates an UC
+     * @param uc_code the code for the uc
+     * @param classes a list of classes in UC
+     * @param enrolled_students a list of students enrolled in the UC
+     */
     UC(const std::string &uc_code, const std::list<std::shared_ptr<Class>> &classes,
        const std::list<std::shared_ptr<Student>> &enrolled_students);
+
+    /**
+     * @brief Getter for classes field
+     * @return a const reference to the classes field
+     */
     const std::list<std::shared_ptr<Class>> &classes() const;
+
+    /**
+     * @brief Removes a class from UC
+     * @param c a pointer to the class to be removed
+     */
     void removeClass(const std::shared_ptr<Class> &c);
+
+    /**
+     * @brief Prints classes in UC
+     */
     void printClasses();
+
+    /**
+     * @brief Getter for enrolled_students field
+     * @return const reference to enrolled students field
+     */
     const std::list<std::shared_ptr<Student>> &enrolled_students() const;
+
+    /**
+     * @brief Prints the students enrolled in the UC
+     */
     void printEnrolledStudents();
+
+    /**
+     * @brief Getter for the UC code
+     * @return
+     */
     std::string uc_code() const;
+
+    /**
+     * @brief Number of students enrolled in UC
+     * @return unsigned int of number of students
+     */
     unsigned countEnrolledStudents() const;
 };
 
