4b6c65a793abeaec3398f103e7ef75c00501958d
Alexandre Silva <128063488+asilva1604@users.noreply.github.com>
2023-10-22 19:28:54 +0100

    Revert "studentsPerYear and UC with most/least students"

--- src/Student.h
+++ src/Student.h
@@ -21,7 +21,6 @@ class Student {
             &codes);
     std::string student_code() const;
     std::string student_name() const;
-    bool operator==(const Student& other) const;
     void enrollInUC(const std::pair<std::string, std::string> &c);
     const std::vector<std::pair<std::string, std::string>> &enrolled_classes() const;
     void printStudent() const;
